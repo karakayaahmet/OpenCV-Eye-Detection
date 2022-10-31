5fefed9aaa0398b200a672c2d26284ec63b35c75
Ahmet Karakaya <karakayahmet00@gmail.com>
2022-11-01 00:21:39 +0300

    resimden göz algılama 2 tamamlandı.

--- resimden-goz-algilama1.py
+++ resimden-goz-algilama1.py
@@ -8,3 +8,11 @@ eye_cascade = cv2.CascadeClassifier("eye.xml")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+
+img2 = img[y:y+h, x:x+w]
+gray2 = gray[y:y+h, x:x+w]
+
+eyes = eye_cascade.detectMultiScale(gray2)
