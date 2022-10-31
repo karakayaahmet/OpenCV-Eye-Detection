7e14c48cc27522266a87f8c76789b9d9e17fd48c
Ahmet Karakaya <karakayahmet00@gmail.com>
2022-11-01 00:31:35 +0300

    videodan göz algılama 1 tamamlandı.

---
+++ videodan-goz-algilama1.py
@@ -0,0 +1,15 @@
+import cv2
+
+cap = cv2.imread("eye.mp4")
+
+face_cascade = cv2.CascadeClassifier("frontalface.xml")
+
+eye_cascade = cv2.CascadeClassifier("eye.xml")
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (480,360))
+    if ret is False:
+        break
+
+
