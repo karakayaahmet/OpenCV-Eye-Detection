2eea57362e8ef25f41ca0436ff18d9b89452d352
Ahmet Karakaya <karakayahmet00@gmail.com>
2022-11-01 00:42:08 +0300

    goz algilama 2 tamamlandı.

--- videodan-goz-algilama1.py
+++ videodan-goz-algilama1.py
@@ -1,7 +1,6 @@
 import cv2
 
-cap = cv2.imread("eye.mp4")
-
+cap = cv2.VideoCapture("eye.mp4")
 face_cascade = cv2.CascadeClassifier("frontalface.xml")
 
 eye_cascade = cv2.CascadeClassifier("eye.xml")
